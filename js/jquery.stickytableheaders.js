e1e32c5409b0cf371f3e7bcdb0bceae6d2a3ee53
zasby <zas@open.by>
2015-04-01 23:22:01 +0300

    Изменил if ((scrollTop > offset.top) && (scrollTop < offset.top + $this.height())) { на if ((scrollTop > offset.top + 100)){ +100 это примерная высота первого tbody

--- js/jquery.stickytableheaders.js
+++ js/jquery.stickytableheaders.js
@@ -79,7 +79,8 @@
 				var scrollTop = base.$window.scrollTop() + newTopOffset;
 				var scrollLeft = base.$window.scrollLeft();
 
-				if ((scrollTop > offset.top) && (scrollTop < offset.top + $this.height())) {
+				/*if ((scrollTop > offset.top) && (scrollTop < offset.top + $this.height())) {*/
+				if ((scrollTop > offset.top + 100)){
 					var newLeft = offset.left - scrollLeft;
 					
 					if (base.isCloneVisible && (newLeft === base.leftOffset) && (newTopOffset === base.topOffset)) {
